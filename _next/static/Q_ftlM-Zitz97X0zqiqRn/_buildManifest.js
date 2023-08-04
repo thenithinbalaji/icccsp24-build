7870e766a75254a59311d0caad93ffe9d5688e6e
thenithinbalaji <thenithinbalaji@gmail.com>
2023-08-04 12:05:27 +0530

    Update paper submission dates

---
+++ _next/static/Q_ftlM-Zitz97X0zqiqRn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/125-af9ae0659559aa1c.js","static/chunks/pages/index-2a0a0d5b65000cc3.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
