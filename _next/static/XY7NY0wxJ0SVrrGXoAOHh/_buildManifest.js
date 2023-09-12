a5f2940b055515e847709667f326aa6be8b26d75
Gokulram A <gokulram2010418@ssn.edu.in>
2023-09-12 18:10:02 +0530

    added organization committee details

---
+++ _next/static/XY7NY0wxJ0SVrrGXoAOHh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/125-af9ae0659559aa1c.js","static/chunks/pages/index-e967737d8d24ec4c.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
