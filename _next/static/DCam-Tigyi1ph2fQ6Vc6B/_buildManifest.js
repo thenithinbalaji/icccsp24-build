8924a7f8700ce89eaf13fa82d30978246cff9087
thenithinbalaji <thenithinbalaji@gmail.com>
2023-09-27 15:53:44 +0530

    27th sept, 2023

---
+++ _next/static/DCam-Tigyi1ph2fQ6Vc6B/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/125-af9ae0659559aa1c.js","static/chunks/pages/index-aaf011ee2b2c8645.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
