5de8e26c952522097ee5d3b125724453e5947bee
Gokulram A <gokulram2010418@ssn.edu.in>
2023-10-19 10:32:57 +0530

    added Dr. to names and changed date from jan 2023 to jan 2024

---
+++ _next/static/Rf9kbWQTr1nED7ZppRcMC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/125-af9ae0659559aa1c.js","static/chunks/pages/index-a13c2a64f18d3380.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
