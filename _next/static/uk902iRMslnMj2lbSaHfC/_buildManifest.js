85c0a932ecda81bb692c5656cca79038e44b25e2
thenithinbalaji <thenithinbalaji@gmail.com>
2024-03-06 15:50:13 +0530

    added speakers, poster session

---
+++ _next/static/uk902iRMslnMj2lbSaHfC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/125-471c233ecc5b1cee.js","static/chunks/pages/index-0811994ada850237.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
